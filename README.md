954b82e92a3211969a6cfc6745f3f51c611f02c7
John Fawcett <jrf0110@gmail.com>
2014-04-23 12:02:32 -0500

    that's basically everything

---
+++ README.md
@@ -0,0 +1,90 @@
+# jQuery Row Expand
+
+Adds a row after the target table row with a `td` whose colspan is set to the parent tables number of columns. The `tr > td` that is added has a `padding: 0` value so you can insert your own HTML without having to worry about much external CSS
+
+![Row Expander](http://storage.j0.hn/row-expander.gif)
+
+__Install:__
+
+```
+bower install jquery.preview
+```
+
+__Usage:__
+
+```html
+<table>
+  <tr>
+    <td>Data 1<td>
+    <td>Data 2<td>
+  </tr>
+  <tr class="expandable">
+    <td>Data 1<td>
+    <td>Data 2<td>
+  </tr>
+  <tr>
+    <td>Data 1<td>
+    <td>Data 2<td>
+  </tr>
+</table>
+```
+
+```javascript
+$(function(){
+  var rowExpander = $('.expandable').rowExpand({
+    template: function(){
+      return [
+        '<div class="expanded-row">'
+      , '  <h2>Ohai!</h2>'
+      , '</div>'
+      ].join('\n');
+    }
+  });
+});
+```
+
+__Options and Defaults:__
+
+```javascript
+{
+  // Markup to insert after target
+  insertHTML:     '<tr><td colspan="{{colspan}}"></td></tr>'
+  // The Element we're actually concerned with within the insertHTML
+, wrapTarget:     'td'
+  // Whether or not to use .css() or .animate() when setting properties
+, animate:        true
+  // Attempt to normalize td's so lib consumer can easily
+  // add their own wrapper
+, targetCss:      { padding: 0 }
+  // Before the wrapper is inserted in DOM, this is the CSS
+, wrapperPreCSS:  { opacity: 0, width: '100%' }
+  // Once the wrapper element is inserted, add this CSS
+, wrapperPostCSS: { opacity: 1 }
+  // If you want to use an existing element as your table row extension,
+  // then pass a selector or jQuery element here
+, srcElement:     null
+  // A function to be called when expanding the table row. The returned
+  // string will be used as the inner markup for
+, template:       null
+};
+```
+
+## Plugin API
+
+This plugin is intendend to applied one element at a time. It returns an interface for dealing with the plugin in the context of the target element.
+
+#### .expand()
+
+Expands the target table row, adding in the necessary elements.
+
+#### .collapse()
+
+Collapses the target table row, removing the previously added elements.
+
+#### .getInnerHTML()
+
+Returns the HTML returned either by `options.template()` or `$( options.srcElement )` that will be inserted into `options.insertHTML > options.wrapTarget`.
+
+#### .getColSpan()
+
+Returns the number of columns the target's parent table has.
---
+++ src/jquery.row-expand.js
@@ -0,0 +1,120 @@
+(function(factory){
+  if ( typeof define === 'function' && define.amd ){
+    // AMD. Register as an anonymous module.
+    define( ['jquery'], factory );
+  } else {
+    // Browser globals
+    factory(jQuery);
+  }
+}(function( $ ){
+  'use strict';
+
+  $.fn.rowExpand = function( options ){
+    var $this = this;
+    var $body = $( document.body );
+
+    var defaults = {
+      // Markup to insert after target
+      insertHTML:     '<tr><td colspan="{{colspan}}"></td></tr>'
+      // The Element we're actually concerned with within the insertHTML
+    , wrapTarget:     'td'
+      // Whether or not to use .css() or .animate() when setting properties
+    , animate:        true
+      // Attempt to normalize td's so lib consumer can easily
+      // add their own wrapper
+    , targetCss:      { padding: 0 }
+      // Before the wrapper is inserted in DOM, this is the CSS
+    , wrapperPreCSS:  { opacity: 0, width: '100%' }
+      // Once the wrapper element is inserted, add this CSS
+    , wrapperPostCSS: { opacity: 1 }
+    };
+
+    options = $.extend( {}, defaults, options );
+
+    if ( typeof options.template !== 'function' )
+    if ( !options.srcElement ){
+      throw new Error('Must provide `template` or `srcElement` properties');
+    }
+
+    if ( typeof options.srcElement === 'string' ){
+      options.srcElement = $( options.srcElement );
+    }
+
+    var plugin = {
+      $this: $this
+
+    , init: function(){
+        return plugin;
+      }
+
+    , getColSpan: function(){
+        return $this.find('td').length;
+      }
+
+    , expand: function(){
+        var initialHeight;
+
+        plugin.collapse();
+
+        plugin.$wrapper = $(
+          options.insertHTML.replace( '{{colspan}}', plugin.getColSpan() )
+        );
+
+        if ( options.wrapTarget ){
+          plugin.$wrapper.find( options.wrapTarget ).html( plugin.getInnerHTML() );
+          plugin.$wrapper.find( options.wrapTarget ).css( options.targetCss );
+        } else {
+          plugin.$wrapper.html( plugin.getInnerHTML() );
+        }
+
+        plugin.$wrapper.css(
+          $.extend( options.wrapperPreCSS, { position: 'absolute' } )
+        );
+
+        $this.after( plugin.$wrapper );
+
+        initialHeight = plugin.$wrapper.height() + 'px';
+
+        plugin.$wrapper.css({
+          height:   0
+        , position: 'static'
+        });
+
+        setTimeout( function(){
+          plugin.$wrapper[ options.animate ? 'animate' : 'css' ](
+            $.extend( options.wrapperPostCSS, { height: initialHeight } )
+          );
+        }, 1 );
+
+        return plugin;
+      }
+
+    , collapse: function(){
+        if ( plugin.$wrapper ){
+          var $el = plugin.$wrapper;
+          var cleanup = function(){
+            $el.remove();
+          };
+
+          $el[ options.animate ? 'animate' : 'css' ](
+            $.extend( options.wrapperPreCSS, { height: 0 } )
+          , options.animate ? cleanup : null
+          );
+
+          if( !options.animate ) cleanup();
+
+          delete plugin.$wrapper;
+        }
+
+        return plugin;
+      }
+
+    , getInnerHTML: function(){
+        if ( options.template ) return options.template();
+        return options.srcElement.html();
+      }
+    };
+
+    return plugin.init();
+  };
+}));
